245e5703d3fb31f3f614bebb703543e20afa4f59
Yuxuan Weng <99111266+regulus1111@users.noreply.github.com>
2023-12-28 08:34:38 +0800

    Delete project directory

--- project/Drone/Drone/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-
-def runoob(request):
-    context = {}
-    context['hello'] = 'Hello World!'
-    return render(request, 'search_form.html', context)
--- project/Drone/static/js/js_usage_detail_6.js
+++
@@ -1,4 +0,0 @@
-function detail_6() {
-	x = document.getElementById("detail_6_button"); //找到元素 
-	x.style.color = "orangered"; //改变样式	
-}
